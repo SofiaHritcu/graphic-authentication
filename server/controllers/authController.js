33a4127bf406f26731acacdef85ebba30cf4b538
SofiaHritcu <61913547+SofiaHritcu@users.noreply.github.com>
2022-03-20 19:53:06 +0200

    feat: implement icons categories endpoint

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const key = require('../config/keys').secret;
