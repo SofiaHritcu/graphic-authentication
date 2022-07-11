97b2a626d861c742df8704a7f6a893f69e8f9dd9
Sofia Hritcu <sofia.hritcu@ailylabs.com>
2022-07-12 00:35:21 +0300

    feat: develop face recogn signup and login on backend

--- server/app.js
+++ server/app.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-const passport = require('passport');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const authRoutes = require('./routes/api/authRoutes');
-const iconsRoutes = require('./routes/api/iconsRoutes');
-const { addPassportStrategy } = require('./middleware/authMiddleware');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const passport = require("passport");
+const methodOverride = require("method-override");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const authRoutes = require("./routes/api/authRoutes");
+const iconsRoutes = require("./routes/api/iconsRoutes");
+const imagesRoutes = require("./routes/api//imagesRoutes");
+const { addPassportStrategy } = require("./middleware/authMiddleware");
 
-// initialize the app
+// -------------- INITIALIZE THE APP -----------
 const app = express();
 
-// middlewares
+// -------------- MIDDLEWARES -----------------
 
 // form data middleware
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false,
-}));
+  })
+);
 
 // json body middleware
 app.use(bodyParser.json());
@@ -31,38 +35,59 @@ app.use(passport.initialize());
 // bring in the passport strategy
 addPassportStrategy(passport);
 
+// method override middleware
+app.use(methodOverride("_method"));
+
 // setting up the static directory static directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+// ------------- DB -------------------------------------
 
 // bring in the database configs and connect to the db
-const db = require('./config/keys').mongoURI;
-mongoose.connect(db, { 
-    useNewUrlParser: true 
-}).then(() => {
-    console.log('Database connected successfully!');
-}).catch(err => {
-    console.log('Unable to connect to the database!');
-});
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database!");
+  });
 
 // add swagger documentation
-const apiDocs = require('./swagger.json');
-app.use('/ga/api/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs, { explorer: true }));
+const apiDocs = require("./swagger.json");
+app.use(
+  "/ga/api/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(apiDocs, { explorer: true })
+);
+
+// --------------------- ROUTES ----------------------
 
 // add users routes
-app.use('/ga/api/users', authRoutes);
+app.use("/ga/api/users", authRoutes);
 
 // add icons routes
-app.use('/ga/api/icons', iconsRoutes);
+app.use("/ga/api/icons", iconsRoutes);
+
+// add iamges routes
+app.use("/ga/api/images", imagesRoutes);
+
+// -------------- ENV ----------------------
 
 // handle production
-if(process.env.NODE_ENV === 'production') {
-    // static folder
-    app.use(express.static(__dirname+'/public'));
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static(__dirname + "/public"));
 
-    // handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  // handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+// ------------------- LISTEN REQUESTS -----------------
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const key = require('../config/keys').secret;
-const User = require('../model/User');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const key = require("../config/keys").secret;
+const User = require("../model/User");
 
 /**
  * @route POST ga/api/users/register
@@ -10,107 +10,120 @@ const User = require('../model/User');
  * @access Public
  */
 module.exports.signup_post = (req, res) => {
-    let { name, userName, email, password, confirmPassword } = req.body;
-    if (password !== confirmPassword) {
-        return res.status(400).json({
-            msg: "Password do not match.",
-        });
+  let { name, userName, email, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      msg: "Password do not match.",
+    });
+  }
+
+  // check for the unique userName
+  User.findOne({
+    userName,
+  }).then((user) => {
+    if (user) {
+      return res.status(400).json({
+        msg: "Username is already taken.",
+      });
     }
+  });
+  // check for the unique userName
+  User.findOne({
+    email,
+  }).then((user) => {
+    if (user) {
+      return res.status(400).json({
+        msg: "Email is already taken.",
+      });
+    }
+  });
+  // user data registration is valid
+  let userToBeRegistered = new User({
+    name,
+    userName,
+    password,
+    email,
+  });
 
-    // check for the unique userName
-    User.findOne({
-        userName
-    }).then(user => {
-        if(user){
-            return res.status(400).json({
-                msg: "Username is already taken.",
-            });
-        }
-    });
-    // check for the unique userName
-    User.findOne({
-        email
-    }).then(user => {
-        if(user){
-            return res.status(400).json({
-                msg: "Email is already taken.",
-            });
-        }
-    });
-    // user data registration is valid
-    let userToBeRegistered = new User({
-        name,
-        userName,
-        password,
-        email,
-    });
+  // hash the password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(userToBeRegistered.password, salt, (err, hash) => {
+      if (err) {
+        throw err;
+      }
 
-    // hash the password
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(userToBeRegistered.password, salt, (err, hash) => {
-            if(err) {
-                throw err;
-            }
-            userToBeRegistered.password = hash;
-            userToBeRegistered.save().then(userRegistered => {
-                return res.status(201).json({
-                    success: true,
-                    msg: "The user has been registered."
-                });
-            });
+      userToBeRegistered.password = hash;
+      userToBeRegistered.save().then((userRegistered) => {
+        console.log("xyzvvvv");
+
+        return res.status(201).json({
+          success: true,
+          msg: "The user has been registered.",
         });
+      });
     });
+  });
+
+  return;
 };
+
+console.log("cccccccccc");
 
 /**
  * @route POST ga/api/users/login
  * @desc Signing in the User
  * @access Public
  */
-module.exports.login_post =  (req, res) => {
-    User.findOne({ userName: req.body.userName }).then(user => {
-        if(!user){
-            return res.status(404).json({
-                msg: "Username was not found.",
-                success: false,
+module.exports.login_post = (req, res) => {
+  console.log("here");
+  User.findOne({ userName: req.body.userName }).then((user) => {
+    if (!user) {
+      return res.status(404).json({
+        msg: "Username was not found.",
+        success: false,
+      });
+    }
+    // if the user exists => password checking
+    bcrypt.compare(req.body.password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // user's password is correct
+        // the token can be sent
+        const payload = {
+          _id: user._id,
+          username: user.username,
+          name: user.name,
+          email: user.email,
+        };
+        jwt.sign(
+          payload,
+          key,
+          { expiresIn: 1000 * 60 * 60 * 24 },
+          (err, token) => {
+            res.status(200).json({
+              success: true,
+              user,
+              token: `Bearer ${token}`,
+              msg: "The user is now logged in.",
             });
-        }
-        // if the user exists => password checking
-        bcrypt.compare(req.body.password, user.password).then(isMatch => {
-            if(isMatch) {
-                // user's password is correct
-                // the token can be sent
-                const payload = {
-                    _id: user._id,
-                    username: user.username,
-                    name: user.name,
-                    email: user.email,
-                };
-                jwt.sign(payload, key, { expiresIn: 1000 * 60 * 60 * 24 }, (err, token) => {
-                    res.status(200).json({
-                        success: true,
-                        user,
-                        token: `Bearer ${token}`,
-                        msg: "The user is now logged in.",
-                    });
-                });
-            } else {
-                return res.status(404).json({
-                    msg: "Incorrect password.",
-                    success: false,
-                });
-            }
-        })
-    })
+          }
+        );
+      } else {
+        return res.status(404).json({
+          msg: "Incorrect password.",
+          success: false,
+        });
+      }
+    });
+  });
 };
 
 /**
- * @route POST ga/api/users/home
- * @desc Return the user's data 
+ * @route GET ga/api/users/home
+ * @desc Return the user's data
  * @access Private
  */
-module.exports.home_get  = (req, res) => {
-    return res.json({
-        user: req.user,
-    });
+module.exports.home_get = (req, res) => {
+  return res.json({
+    user: req.user,
+  });
 };
